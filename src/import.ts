63d29b4364e71bcf214ece0bcb9e7e5a6b6ae1aa
fengmk2 <suqian.yf@antgroup.com>
2024-12-24 20:26:52 +0800

    fix: should try to use esm module first

--- src/import.ts
+++ src/import.ts
@@ -1,6 +1,6 @@
 import { debuglog } from 'node:util';
 import { createRequire } from 'node:module';
-import { pathToFileURL } from 'node:url';
+import { pathToFileURL, fileURLToPath } from 'node:url';
 import path from 'node:path';
 import fs from 'node:fs';
 
@@ -39,18 +39,90 @@ function isSupportTypeScript() {
   return _supportTypeScript;
 }
 
-function tryToGetTypeScriptMainFile(pkg: any, baseDir: string): string | undefined {
+function tryToResolveFromFile(filepath: string): string | undefined {
+  // "type": "module", try index.mjs then index.js
+  const type = isESM ? 'module' : 'commonjs';
+  let mainIndexFile = '';
+  if (type === 'module') {
+    mainIndexFile = filepath + '.mjs';
+    if (fs.existsSync(mainIndexFile)) {
+      debug('[tryToResolveFromFile] %o, use index.mjs, type: %o', mainIndexFile, type);
+      return mainIndexFile;
+    }
+    mainIndexFile = filepath + '.js';
+    if (fs.existsSync(mainIndexFile)) {
+      debug('[tryToResolveFromFile] %o, use index.js, type: %o', mainIndexFile, type);
+      return mainIndexFile;
+    }
+  } else {
+    // "type": "commonjs", try index.js then index.cjs
+    mainIndexFile = filepath + '.cjs';
+    if (fs.existsSync(mainIndexFile)) {
+      debug('[tryToResolveFromFile] %o, use index.cjs, type: %o', mainIndexFile, type);
+      return mainIndexFile;
+    }
+    mainIndexFile = filepath + '.js';
+    if (fs.existsSync(mainIndexFile)) {
+      debug('[tryToResolveFromFile] %o, use index.js, type: %o', mainIndexFile, type);
+      return mainIndexFile;
+    }
+  }
+
+  if (!isSupportTypeScript()) {
+    return;
+  }
+
+  // for the module under development
+  mainIndexFile = filepath + '.ts';
+  if (fs.existsSync(mainIndexFile)) {
+    debug('[tryToResolveFromFile] %o, use index.ts, type: %o', mainIndexFile, type);
+    return mainIndexFile;
+  }
+}
+
+function tryToResolveByDirnameFromPackage(dirname: string, pkg: any): string | undefined {
   // try to read pkg.main or pkg.module first
   // "main": "./dist/commonjs/index.js",
   // "module": "./dist/esm/index.js"
   const defaultMainFile = isESM ? pkg.module ?? pkg.main : pkg.main;
   if (defaultMainFile) {
-    const mainIndexFilePath = path.join(baseDir, defaultMainFile);
+    const mainIndexFilePath = path.join(dirname, defaultMainFile);
     if (fs.existsSync(mainIndexFilePath)) {
-      debug('[tryToGetTypeScriptMainFile] %o, use pkg.main or pkg.module: %o, isESM: %s',
+      debug('[tryToResolveByDirnameFromPackage] %o, use pkg.main or pkg.module: %o, isESM: %s',
         mainIndexFilePath, defaultMainFile, isESM);
-      return;
+      return mainIndexFilePath;
     }
+  }
+
+  // "type": "module", try index.mjs then index.js
+  const type = pkg?.type ?? (isESM ? 'module' : 'commonjs');
+  if (type === 'module') {
+    const mainIndexFilePath = path.join(dirname, 'index.mjs');
+    if (fs.existsSync(mainIndexFilePath)) {
+      debug('[tryToResolveByDirnameFromPackage] %o, use index.mjs, pkg.type: %o', mainIndexFilePath, type);
+      return mainIndexFilePath;
+    }
+    const mainIndexMjsFilePath = path.join(dirname, 'index.js');
+    if (fs.existsSync(mainIndexMjsFilePath)) {
+      debug('[tryToResolveByDirnameFromPackage] %o, use index.js, pkg.type: %o', mainIndexMjsFilePath, type);
+      return mainIndexMjsFilePath;
+    }
+  } else {
+    // "type": "commonjs", try index.cjs then index.js
+    const mainIndexFilePath = path.join(dirname, 'index.cjs');
+    if (fs.existsSync(mainIndexFilePath)) {
+      debug('[tryToResolveByDirnameFromPackage] %o, use index.cjs, pkg.type: %o', mainIndexFilePath, type);
+      return mainIndexFilePath;
+    }
+    const mainIndexCjsFilePath = path.join(dirname, 'index.js');
+    if (fs.existsSync(mainIndexCjsFilePath)) {
+      debug('[tryToResolveByDirnameFromPackage] %o, use index.js, pkg.type: %o', mainIndexCjsFilePath, type);
+      return mainIndexCjsFilePath;
+    }
+  }
+
+  if (!isSupportTypeScript()) {
+    return;
   }
 
   // for the module under development
@@ -60,33 +132,50 @@ function tryToGetTypeScriptMainFile(pkg: any, baseDir: string): string | undefin
   //     ".": "./src/index.ts"
   //   }
   // }
-  const mainIndexFile = pkg.tshy?.exports?.['.'];
-  if (mainIndexFile) {
-    const mainIndexFilePath = path.join(baseDir, mainIndexFile);
-    if (fs.existsSync(mainIndexFilePath)) {
-      return mainIndexFilePath;
-    }
+  const mainIndexFile = pkg.tshy?.exports?.['.'] ?? 'index.ts';
+  const mainIndexFilePath = path.join(dirname, mainIndexFile);
+  if (fs.existsSync(mainIndexFilePath)) {
+    return mainIndexFilePath;
+  }
+}
+
+function tryToResolveByDirname(dirname: string): string | undefined {
+  const pkgFile = path.join(dirname, 'package.json');
+  if (fs.existsSync(pkgFile)) {
+    const pkg = JSON.parse(fs.readFileSync(pkgFile, 'utf-8'));
+    return tryToResolveByDirnameFromPackage(dirname, pkg);
   }
 }
 
 export function importResolve(filepath: string, options?: ImportResolveOptions) {
-  // support typescript import on absolute path
-  if (path.isAbsolute(filepath) && isSupportTypeScript()) {
-    const pkgFile = path.join(filepath, 'package.json');
-    if (fs.existsSync(pkgFile)) {
-      const pkg = JSON.parse(fs.readFileSync(pkgFile, 'utf-8'));
-      const mainFile = tryToGetTypeScriptMainFile(pkg, filepath);
-      if (mainFile) {
-        debug('[importResolve] %o, use typescript main file: %o', filepath, mainFile);
-        return mainFile;
+  let moduleFilePath: string | undefined;
+  if (path.isAbsolute(filepath)) {
+    const stat = fs.statSync(filepath, { throwIfNoEntry: false });
+    if (stat?.isDirectory()) {
+      moduleFilePath = tryToResolveByDirname(filepath);
+      if (moduleFilePath) {
+        debug('[importResolve] %o => %o', filepath, moduleFilePath);
+        return moduleFilePath;
+      }
+    }
+    if (!stat) {
+      moduleFilePath = tryToResolveFromFile(filepath);
+      if (moduleFilePath) {
+        debug('[importResolve] %o => %o', filepath, moduleFilePath);
+        return moduleFilePath;
       }
     }
   }
-  const cwd = process.cwd();
-  const paths = options?.paths ?? [ cwd ];
-  const moduleFilePath = getRequire().resolve(filepath, {
-    paths,
-  });
+
+  if (isESM) {
+    moduleFilePath = fileURLToPath(import.meta.resolve(filepath));
+  } else {
+    const cwd = process.cwd();
+    const paths = options?.paths ?? [ cwd ];
+    moduleFilePath = require.resolve(filepath, {
+      paths,
+    });
+  }
   debug('[importResolve] %o, options: %o => %o', filepath, options, moduleFilePath);
   return moduleFilePath;
 }
@@ -94,22 +183,10 @@ export function importResolve(filepath: string, options?: ImportResolveOptions) 
 export async function importModule(filepath: string, options?: ImportModuleOptions) {
   const moduleFilePath = importResolve(filepath, options);
   let obj: any;
-  if (typeof require === 'function') {
-    // commonjs
-    obj = require(moduleFilePath);
-    debug('[importModule] require %o => %o', filepath, obj);
-    if (obj?.__esModule === true && 'default' in obj) {
-      // 兼容 cjs 模拟 esm 的导出格式
-      // {
-      //   __esModule: true,
-      //   default: { fn: [Function: fn], foo: 'bar', one: 1 }
-      // }
-      obj = obj.default;
-    }
-  } else {
+  if (isESM) {
     // esm
-    debug('[importModule] await import start: %o', filepath);
     const fileUrl = pathToFileURL(moduleFilePath).toString();
+    debug('[importModule] await import start: %o', fileUrl);
     obj = await import(fileUrl);
     debug('[importModule] await import end: %o => %o', filepath, obj);
     // {
@@ -150,6 +227,18 @@ export async function importModule(filepath: string, options?: ImportModuleOptio
       if ('default' in obj) {
         obj = obj.default;
       }
+    }
+  } else {
+    // commonjs
+    obj = require(moduleFilePath);
+    debug('[importModule] require %o => %o', filepath, obj);
+    if (obj?.__esModule === true && 'default' in obj) {
+      // 兼容 cjs 模拟 esm 的导出格式
+      // {
+      //   __esModule: true,
+      //   default: { fn: [Function: fn], foo: 'bar', one: 1 }
+      // }
+      obj = obj.default;
     }
   }
   debug('[importModule] return %o => %o', filepath, obj);
